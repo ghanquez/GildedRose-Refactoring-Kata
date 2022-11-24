5532a352d9f775abf385ce04ff300319494d54f5
Gaël Hanquez <7419028+ghanquez@users.noreply.github.com>
2022-11-24 12:57:14 +0000

    test: add first tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -7,3 +7,21 @@ describe('Gilded Rose', () => {
     expect(items[0].name).toBe('foo');
   });
 });
+
+describe('Legendary objects Sulfuras, Hand of Ragnaros', () => {
+
+})
+
+describe('Backstage passes', () => {
+
+})
+
+describe('Aged Brie', () => {
+  it('should increase the quality after update', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 20, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('Aged Brie');
+    expect(items[0].sellIn).toBe(19);
+    expect(items[0].quality).toBe(3);
+  });
+})
